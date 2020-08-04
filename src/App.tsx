e529d1c75026e0f481bc8f0f5878f3f26775c994
maple <2313694690@qq.com>
2020-08-04 22:45:26 +0800

    创建 views 目录 封装页面

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import Layout from 'components/Layout';
 
+import Tags from 'views/Tags'
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
 
 function App() {
   return (
@@ -30,34 +33,6 @@ function App() {
 
     </Router>
   );
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>
-    </Layout>
-  );
-}
-
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页面</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页面</h2>
-    </Layout>
-  );
-}
-
-function NoMatch() {
-  return <h2>页面不存在，请重新输入地址！</h2>;
 }
 
 export default App;
