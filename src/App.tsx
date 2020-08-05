89f7703895a41611d2d1a896e719cbdfd2fb4e83
maple <2313694690@qq.com>
2020-08-05 10:23:56 +0800

    Money.tsx 之 TagsSection UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,36 +6,42 @@ import {
   Redirect,
 } from 'react-router-dom';
 
-import Tags from 'views/Tags'
-import Money from './views/Money';
-import Statistics from './views/Statistics';
-import NoMatch from './views/NoMatch';
+import Tags from 'views/Tags';
+import Money from 'views/Money';
+import Statistics from 'views/Statistics';
+import NoMatch from 'views/NoMatch';
+import styled from 'styled-components';
+
+
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
-  let optionsState;
   return (
-    <Router>
+    <AppWrapper>
+      <Router>
 
+        <Switch>
+          <Route exact path="/tags">
+            <Tags/>
+          </Route>
+          <Route exact path="/money">
+            <Money/>
+          </Route>
+          <Route exact path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="/money"/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
 
-
-      <Switch>
-        <Route exact path="/tags">
-          <Tags/>
-        </Route>
-        <Route exact path="/money">
-          <Money/>
-        </Route>
-        <Route exact path="/statistics">
-          <Statistics/>
-        </Route>
-        <Redirect exact from="/" to="/money"/>
-        <Route path="*">
-          <NoMatch/>
-        </Route>
-      </Switch>
-
-    </Router>
-  );
+      </Router>
+    </AppWrapper>
+  )
+    ;
 }
 
 export default App;
