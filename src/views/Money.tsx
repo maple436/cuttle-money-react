a099331c5588eb5d744f63d6f95d4dbac7ed774d
maple <2313694690@qq.com>
2020-08-05 10:45:31 +0800

    Money.tsx UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,6 +4,11 @@ import styled from 'styled-components';
 
 
 const TagsSection = styled.section`
+       flex-grow: 1;
+       display:flex; 
+       flex-direction: column;
+       justify-content: flex-end; 
+       align-items: flex-start;
        background: #FFFFFF; 
        padding: 12px 16px;
         > ol { 
@@ -118,9 +123,14 @@ const NumberPadSection = styled.section`
         }
 `;
 
+const MyLayout = styled(Layout)`
+  display:flex;
+  flex-direction: column;
+`
+
 function Money() {
   return (
-    <Layout>
+    <MyLayout>
       <TagsSection>
         <ol>
           <li>衣</li>
@@ -164,7 +174,7 @@ function Money() {
         </div>
       </NumberPadSection>
 
-    </Layout>
+    </MyLayout>
   );
 }
 
