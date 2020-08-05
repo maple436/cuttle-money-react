1942e78d1a013d9adb7c224e7aeb1113268c5805
maple <2313694690@qq.com>
2020-08-05 20:56:29 +0800

    完成标签的选择和新增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection = styled.section`
+
+const Wrapper = styled.section`
        flex-grow: 1;
        display:flex; 
        flex-direction: column;
@@ -17,8 +19,12 @@ const TagsSection = styled.section`
                padding: 3px 18px; 
                font-size: 14px;
                margin: 8px 12px;
+               &.selected{
+                  background: #f60;
            }
         }
+      }
+        
         > button{
             background:none;
             border: none; 
@@ -27,7 +33,53 @@ const TagsSection = styled.section`
             color: #666;
             margin-top: 8px;
           }
-  
+          
 `;
+
+const TagsSection: React.FC = () => {
+  const [tags,setTags]=useState<string[]>(['衣','食','住','行'])
+  const [selectedTags,setSelectedTags]=useState<string[]>([])
+
+  const onAddTag=()=>{
+    const tagName=window.prompt('请输入标签名:');
+    if(tagName){
+      const index=tags.indexOf(tagName);
+      if(index>=0){
+        return window.alert('标签名重复，请重新输入!!!')
+      }else{
+        setTags([...tags,tagName]);
+      }
+    }else if(!tagName){
+      return ;
+    }
+  }
+
+  const onToggleTag=(tag:string)=>{
+    const index=selectedTags.indexOf(tag);
+    if(index>=0){
+      //如果 tag 已被选中，就复制所有没有被选中的 tag，作为新的 selectedTag
+      setSelectedTags(selectedTags.filter(t=>t!==tag));
+    }else{
+      setSelectedTags([...selectedTags,tag])
+    }
+  }
+
+  const getClass=(tag:string)=>selectedTags.indexOf(tag)>=0?'selected':'';
+
+
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag=>
+          <li key={tag}
+          onClick={()=>{onToggleTag(tag)}}
+          className={getClass(tag)}>{tag}</li>
+        )}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
+
 
 export {TagsSection};
