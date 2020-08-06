3502e5110b2b836475d1e2c8d93813dd313324b3
maple <2313694690@qq.com>
2020-08-06 14:58:21 +0800

    完成备注功能

--- src/views/Money/NotesSection.tsx
+++ src/views/Money/NotesSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useRef, useState} from 'react';
 
-const NotesSection = styled.section`
+const Wrapper = styled.section`
       background: #f5f5f5;
       padding: 0 16px;
       font-size: 14px;
@@ -18,4 +19,24 @@ const NotesSection = styled.section`
       }
 `;
 
-export {NotesSection}
+const NoteSection: React.FC = () => {
+  const [note, setNote] = useState('');
+  const refInput = useRef<HTMLInputElement>(null);
+  const onBlur = () => {
+    if (refInput.current !== null) {
+      setNote(refInput.current.value);
+    }
+  };
+  return (
+    <Wrapper>
+      <label>
+        <span>备注</span>
+        <input type="text" placeholder="在这里添加备注"
+               ref={refInput} defaultValue={note} onBlur={onBlur}
+        />
+      </label>
+    </Wrapper>
+  );
+};
+
+export {NoteSection};
