235671a716375252cc6ccfeebf29ededb36485a9
maple <2313694690@qq.com>
2020-08-09 10:59:58 +0800

    创建 createdId 让id自增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
-import {useTags} from '../../useTags';
+import {useTags} from 'useTags';
+import {createId} from 'lib/cratedId';
 
 
 const Wrapper = styled.section`
@@ -50,7 +51,7 @@ const TagsSection: React.FC<Props> = (props) => {
   const onAddTag = () => {
     const tagName = window.prompt('请输入标签名:');
     if(tagName!==null){
-      setTags([...tags,{id:Math.random(),name:tagName}])
+      setTags([...tags,{id:createId(),name:tagName}])
     }
   };
 
